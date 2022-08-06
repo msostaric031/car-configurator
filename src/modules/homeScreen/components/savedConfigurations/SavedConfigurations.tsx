b92ac4c1f2216058108629027686bc25b44e0f73
Marko <54621273+msostaric-hub@users.noreply.github.com>
2022-08-06 13:44:41 +0200

    Edited home and login screen

--- src/modules/homeScreen/components/savedConfigurations/SavedConfigurations.tsx
+++ src/modules/homeScreen/components/savedConfigurations/SavedConfigurations.tsx
@@ -2,8 +2,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './SavedConfigurations.styles';
+import { getDownloadURL, getStorage, ref } from 'firebase/storage';
+import { initializeApp } from 'firebase/app';
+import { config } from 'config';
+
+initializeApp(config.firebaseConfig);
+
+const storage = getStorage();
+
+const imgRef = ref(
+  storage,
+  'gs://car-configurator-117c8.appspot.com/assets/audiRS5/front left-2.png',
+);
 
 export const SavedConfigurations: React.FC = () => {
+  getDownloadURL(ref(imgRef)).then((url) => {
+    const imgPoint = document.getElementById('carImg');
+    imgPoint?.setAttribute('src', url);
+  });
+
   return (
     <div css={styles.container}>
       <div css={styles.container__header}>
@@ -13,7 +30,15 @@ export const SavedConfigurations: React.FC = () => {
         </Link>
       </div>
       <div css={styles.configurations__container}>
-        <div css={styles.img}></div>
+        <div css={styles.img}>
+          <img
+            src=""
+            alt="Empty container img"
+            id="carImg"
+            css={styles.car__img}
+          />
+        </div>
+
         <p>
           You haven't configured any cars yet.
           <Link to="/carselect">You may choose to configure some now.</Link>
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,5 +1,6 @@
 /** @jsxImportSource @emotion/react */
-import { SavedConfigurations } from 'modules/homeScreen';
+
+import { SavedConfigurations } from 'modules';
 import React from 'react';
 
 export const Home: React.FC = () => {
